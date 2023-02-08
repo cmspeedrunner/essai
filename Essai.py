ac1e3a181599ca027469f6c4c79a3a4990db65ce
Cm <109184310+cmspeedrunner@users.noreply.github.com>
2023-02-08 23:57:41 +0000

    Add files via upload

--- Essai.py
+++ Essai.py
@@ -1,33 +1,30 @@
 
 import wikipedia
 import pyperclip
-
-choide = int(input("Welcome to Essai!\nType in a topic and allow us to write an essay in mere seconds!\nType 1 to go to breif description\nType 2 to go to more detailed\ntype 3 to go for the most detailed\ntype 4 to search if you have error\n"))
+import colorama
+choide = int(input("Welcome to Essai!\nType in a topic and allow us to write an essay in mere seconds!\nType"+colorama.Fore.GREEN+" 1 "+colorama.Fore.WHITE+"to go to breif description\nType"+colorama.Fore.BLUE+" 2 "+colorama.Fore.WHITE+"to go to more detailed\ntype"+colorama.Fore.YELLOW+" 3 "+colorama.Fore.WHITE+"to go for the most detailed\ntype"+colorama.Fore.RED+" 4 "+colorama.Fore.WHITE+"to search if you have error\n"))
 if choide == 1:
 
     wiki = input("Enter your topic!\n")
-    summary = wikipedia.summary(wiki, sentences=2)
+    summary = wikipedia.summary(wiki, sentences=2,auto_suggest=True)
     print("")
-    print(summary)
+    print(colorama.Fore.GREEN+summary+colorama.Fore.WHITE)
     pyperclip.copy(summary)
 if choide == 2:
 
     wiki = input("Enter your topic!\n")
-    summary = wikipedia.summary(wiki, sentences=10)
+    summary = wikipedia.summary(wiki, sentences=10,auto_suggest=True)
     print("")
-    print(summary)
+    print(colorama.Fore.GREEN+summary+colorama.Fore.WHITE)
     pyperclip.copy(summary)
 if choide == 3:
         wiki = input("Enter your topic!\n")
-        summary = wikipedia.summary(wiki, sentences=1000)
+        summary = wikipedia.summary(wiki, sentences=1000,auto_suggest=True)
         print("")
-        print(summary)
+        print(colorama.Fore.GREEN+summary+colorama.Fore.WHITE)
         pyperclip.copy(summary)
 if choide == 4:
     wiki = input("Enter your topic you are having errors with.\n")
     mainwiki = wikipedia.search(wiki)
     print(mainwiki)
     print("\nIf you are getting an error searching '"+wiki+"' then try using some specific identifiers like those above\n")
-elif:
-    print("INVALID CHOICE")
-    
